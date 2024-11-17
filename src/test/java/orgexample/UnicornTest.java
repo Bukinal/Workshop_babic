2dabeda2b21b5a36b75e10dbf6bae925a41689e0
Alena Bukina <bukina102@mail.ru>
2024-11-17 13:50:42 +0300

    adding tests for unicorn

--- src/test/java/orgexample/UnicornTest.java
+++ src/test/java/orgexample/UnicornTest.java
@@ -12,13 +12,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
 
 public class UnicornTest {
     @BeforeAll
     public static void setup() {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-        RestAssured.baseURI = "https://crudcrud.com/api/073c293746ea489292e82e4b87b4c1a7";
+        RestAssured.baseURI = "https://crudcrud.com/api/683037dd56d44ff5b3e2e4a448fe9532";
     }
 
     @Test
@@ -76,7 +77,7 @@ public class UnicornTest {
     public void userShouldBeAbleUpdateUnicorn() {
         Map<String, String> jsonBody = new HashMap<>();
         jsonBody.put("name", "Artem");
-        jsonBody.put("color", "Green");
+        jsonBody.put("color", "Red");
         System.out.println(jsonBody);
 
         String id = given()
@@ -101,10 +102,14 @@ public class UnicornTest {
                 .assertThat().statusCode(HttpStatus.SC_OK);
 
         given()
-                .get("/unicorn/" + id)
+                .contentType("application/json")
+                .accept("application/json")
+                .body(jsonBody)
+                .get("/unicorns/" + id)
                 .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_NOT_FOUND);
+                .statusCode(200)
+                .body("name", equalTo(jsonBody.get("name")))
+                .body("_id", equalTo((id)))
+                .body("color", equalTo(jsonBody.get("color")));
     }
-
 }
